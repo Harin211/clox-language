f815ff520d17c86a8ca2e2be5c1cae92a3b49621
Harin V <165261397+Harin211@users.noreply.github.com>
2025-09-13 16:03:50 +1000

    Created the skeleton for the virtual machine that will help run our code as part of the interpreter.

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
+
+    initVM();
 
     Chunk chunk;
     initChunk(&chunk);
@@ -14,9 +17,11 @@ int main(int argc, const char *argv[]) {
     // write the index of the constant
     writeChunk(&chunk, constant, 123);
     writeChunk(&chunk, OP_RETURN, 123);
-    
-    disassembleChunk(&chunk, "test chunk");
 
+    disassembleChunk(&chunk, "test chunk");
+    
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
